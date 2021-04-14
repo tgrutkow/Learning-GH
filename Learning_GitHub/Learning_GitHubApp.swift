f5f9e0bd730ec3bc1a1bfbdbd50b549a1e40a2aa
Thomas Grutkowski <tomfc7@gmail.com>
2021-04-13 20:46:18 -0600

    Initial Commit

---
+++ Learning_GitHub/Learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitHubApp.swift
+//  Learning_GitHub
+//
+//  Created by Thomas Grutkowski on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
